1f28444007a2fb061483fb0a18684497efa8bed6
Vinay Kumar Goud <144577819+vinay1723@users.noreply.github.com>
2026-03-03 21:44:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your code here
+function majorityElement(arr) {
+			let count = 0;
+			let currNum;
+			for(let num of arr) {
+				if (count === 0) {
+					currNum = num;
+				}
+				count += currNum === num ? 1:-1;
+			}
+
+			return currNum;
+		}
+		majorityElement([2,1,2]);
